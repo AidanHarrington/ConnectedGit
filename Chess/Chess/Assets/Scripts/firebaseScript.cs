2fcd5d2650dc7dbb65a53cabdcae676dd079c6c3
AidanHarrington <32704534+AidanHarrington@users.noreply.github.com>
2021-02-01 14:34:19 +0100

    Chess + Background
    Created simple chess as well as changing the background every other game. Beginning of Task 7.

---
+++ Chess/Chess/Assets/Scripts/firebaseScript.cs
@@ -0,0 +1,211 @@
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Storage;
+using Firebase.Extensions;
+using Firebase.Unity.Editor;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using System.IO;
+
+public class firebaseScript : MonoBehaviour
+{
+    public bool signedin = false;
+    DatabaseReference reference;
+
+    //reference to the storage bucket
+    FirebaseStorage storage;
+
+    //main data dictionary
+    Dictionary<string, object> myDataDictionary;
+
+    FirebaseAuth auth;
+
+    string email = "aidanh@gmail.com";
+    string password = "User123";
+    string path = "https://aidanharringtonunity.firebaseio.com/";
+
+    private Sprite LoadSprite(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return null;
+        if (System.IO.File.Exists(path))
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            return sprite;
+        }
+        return null;
+    }
+
+    public IEnumerator downloadAndSaveImage()
+    {
+        string pathToSaveIn = Application.persistentDataPath;
+        string[] backgroundImages = new string[] { "gs://aidanharringtonunity.appspot.com/wood1.jpg",
+            "gs://aidanharringtonunity.appspot.com/wood2.jpg",
+            "gs://aidanharringtonunity.appspot.com/wood3.jpg",
+            "gs://aidanharringtonunity.appspot.com/wood4.jpg"};
+        string randomBackgroundImage = backgroundImages[UnityEngine.Random.Range(0, backgroundImages.Length)];
+
+        storage = FirebaseStorage.DefaultInstance;
+
+        // Create local filesystem URL
+
+        string filename = Application.persistentDataPath + "/BackgroundImage.jpg";
+
+        StorageReference storage_ref = storage.GetReferenceFromUrl(randomBackgroundImage);
+
+        // Start downloading a file
+        Task task = storage_ref.GetFileAsync(filename,
+          new Firebase.Storage.StorageProgress<DownloadState>((DownloadState state) => {
+              // called periodically during the download
+              Debug.Log(String.Format(
+                "Progress: {0} of {1} bytes transferred.",
+                state.BytesTransferred,
+                state.TotalByteCount
+              ));
+          }), CancellationToken.None);
+
+        task.ContinueWith(resultTask => {
+            if (!resultTask.IsFaulted && !resultTask.IsCanceled)
+            {
+                Debug.Log("Download finished.");
+            }
+        });
+
+        Debug.Log(filename);
+
+        yield return new WaitUntil(() => task.IsCompleted);
+
+        Sprite backgroundImage = LoadSprite(filename);
+        GameObject.Find("BackgroundImage").GetComponent<SpriteRenderer>().sprite = backgroundImage;
+
+        yield return null;
+    }
+
+    public IEnumerator downloadAndSaveGreenPieces()
+    {
+        string pathToSaveIn = Application.persistentDataPath;
+        string[] greenPieces = new string[] { "gs://aidanharringtonunity.appspot.com/green/black_bishop.png",
+            "gs://aidanharringtonunity.appspot.com/green/black_king.png",
+            "gs://aidanharringtonunity.appspot.com/green/black_knight.png",
+            "gs://aidanharringtonunity.appspot.com/green/black_pawn.png",
+            "gs://aidanharringtonunity.appspot.com/green/black_queen.png",
+            "gs://aidanharringtonunity.appspot.com/green/black_rook.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_bishop.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_king.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_knight.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_pawn.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_queen.png",
+            "gs://aidanharringtonunity.appspot.com/green/white_rook.png",
+            "gs://aidanharringtonunity.appspot.com/green/board.png"};
+        
+        foreach(var element in greenPieces)
+        {
+
+        }
+
+        storage = FirebaseStorage.DefaultInstance;
+
+        // Create local filesystem URL
+
+        string filename = Application.persistentDataPath + "/BackgroundImage.jpg";
+
+        StorageReference storage_ref = storage.GetReferenceFromUrl(randomBackgroundImage);
+
+        // Start downloading a file
+        Task task = storage_ref.GetFileAsync(filename,
+          new Firebase.Storage.StorageProgress<DownloadState>((DownloadState state) => {
+              // called periodically during the download
+              Debug.Log(String.Format(
+                "Progress: {0} of {1} bytes transferred.",
+                state.BytesTransferred,
+                state.TotalByteCount
+              ));
+          }), CancellationToken.None);
+
+        task.ContinueWith(resultTask => {
+            if (!resultTask.IsFaulted && !resultTask.IsCanceled)
+            {
+                Debug.Log("Download finished.");
+            }
+        });
+
+        Debug.Log(filename);
+
+        yield return new WaitUntil(() => task.IsCompleted);
+
+        Sprite backgroundImage = LoadSprite(filename);
+        GameObject.Find("BackgroundImage").GetComponent<SpriteRenderer>().sprite = backgroundImage;
+
+        yield return null;
+    }
+
+    public IEnumerator clearFirebase()
+    {
+        Task removeAllRecords = reference.RemoveValueAsync().ContinueWithOnMainThread(
+            rmAllRecords =>
+            {
+                if (rmAllRecords.IsCompleted)
+                {
+                    Debug.Log("Database clear");
+                }
+            });
+
+        yield return new WaitUntil(() => removeAllRecords.IsCompleted);
+
+    }
+    public IEnumerator initFirebase(string path)
+    {
+        if (!signedin)
+        {
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://aidanharringtonunity.firebaseio.com/");
+            reference = FirebaseDatabase.DefaultInstance.RootReference;
+            yield return signInToFirebase();
+            Debug.Log("Firebase Initialized!");
+            yield return true;
+            signedin = true;
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+    IEnumerator signInToFirebase()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        //the outside task is a DIFFERENT NAME to the anonymous inner class
+        Task signintask = auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(
+             signInTask =>
+             {
+                 if (signInTask.IsCanceled)
+                 {
+                     //write cancelled in the console
+                     Debug.Log("Cancelled!");
+                     return;
+                 }
+                 if (signInTask.IsFaulted)
+                 {
+                     //write the actual exception in the console
+                     Debug.Log("Something went wrong!" + signInTask.Exception);
+                     return;
+                 }
+
+                 Firebase.Auth.FirebaseUser loggedInUser = signInTask.Result;
+                 Debug.Log("User " + loggedInUser.DisplayName + " has logged in!");
+             }
+            );
+        yield return new WaitUntil(() => signintask.IsCompleted);
+
+        Debug.Log("User has signed in");
+    }
+}
