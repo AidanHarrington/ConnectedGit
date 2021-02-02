67a0236098fb534126b445978515089c1f11c974
AidanHarrington <32704534+AidanHarrington@users.noreply.github.com>
2021-02-02 23:04:37 +0100

    Pieces downloading + progress
    Pieces are downloading and spawned in correct place. progress bar working with timer to 5 sec

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public GameObject chessPiece;
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] playerBlack = new GameObject[16];
+    private GameObject[] playerWhite = new GameObject[16];
+
+    private string currentPlayer = "white";
+    public bool gameOver = false;
+
+    public float p1CurrentTime, p2CurrentTime;
+
+    [SerializeField] 
+    Text p1Time, p2Time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1CurrentTime = 120f;
+        p2CurrentTime = 120f;
+        playerWhite = new GameObject[]
+        {
+            Create ("whiteRook", 0,0), Create ("whiteKnight", 1,0), Create ("whiteBishop", 2,0), Create ("whiteQueen", 3,0),
+            Create ("whiteKing", 4,0), Create ("whiteBishop", 5,0), Create ("whiteKnight", 6,0), Create ("whiteRook", 7,0),
+            Create ("whitePawn", 0,1), Create ("whitePawn", 1,1), Create ("whitePawn", 2,1), Create ("whitePawn", 3,1),
+            Create ("whitePawn", 4,1), Create ("whitePawn", 5,1), Create ("whitePawn", 6,1), Create ("whitePawn", 7,1)
+        };
+
+        playerBlack = new GameObject[]
+    {
+            Create ("blackRook", 0,7), Create ("blackKnight", 1,7), Create ("blackBishop", 2,7), Create ("blackQueen", 3,7),
+            Create ("blackKing", 4,7), Create ("blackBishop", 5,7), Create ("blackKnight", 6,7), Create ("blackRook", 7,7),
+            Create ("blackPawn", 0,6), Create ("blackPawn", 1,6), Create ("blackPawn", 2,6), Create ("blackPawn", 3,6),
+            Create ("blackPawn", 4,6), Create ("blackPawn", 5,6), Create ("blackPawn", 6,6), Create ("blackPawn", 7,6)
+    };
+
+        for (int i = 0; i < playerBlack.Length; i++)
+        {
+            setPosition(playerBlack[i]);
+            setPosition(playerWhite[i]);
+        }
+    }
+    public GameObject Create(string name, int x, int y)
+    {
+        GameObject obj = Instantiate(chessPiece, new Vector3(0, 0, -1), Quaternion.identity);
+        ChessMan cm = obj.GetComponent<ChessMan>();
+        obj.tag = name;
+        cm.name = name;
+        cm.SetXBoard(x);
+        cm.SetYBoard(y);
+        cm.Activate();
+        return obj;
+    }
+    public void setPosition(GameObject obj)
+    {
+        ChessMan cm = obj.GetComponent<ChessMan>();
+        positions[cm.GetXBoard(), cm.getYBoard()] = obj;
+    }
+
+    public void SetPositionEmpty(int x, int y)
+    {
+        positions[x, y] = null;
+    }
+
+    public GameObject GetPosition(int x, int y)
+    {
+        return positions[x, y];
+    }
+
+    public bool PositionOnBoard(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1)) {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+
+    public string GetCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+
+    public bool IsGameOver()
+    {
+        return gameOver;
+    }
+
+    public void NextTurn()
+    {
+        if (currentPlayer == "white")
+        {
+            currentPlayer = "black";
+        }
+
+        else
+        {
+            currentPlayer = "white";
+        }
+    }
+
+    public void Update()
+    {
+        if (gameOver == true)
+        {
+            StartCoroutine(LoadMainMenu());
+        }
+
+        if (currentPlayer == "white" && gameOver == false)
+        {
+            p1CurrentTime -= 1 * Time.deltaTime;
+        }
+
+        else if (currentPlayer == "black" && gameOver == false)
+        {
+            p2CurrentTime -= 1 * Time.deltaTime;
+        }
+
+        if (p1CurrentTime == 0 || p1CurrentTime <= 0)
+        {
+            p1CurrentTime = 0;
+            gameOver = true;
+            GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+            GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = "black is the winner";
+        }
+
+        else if (p2CurrentTime == 0 || p2CurrentTime <= 0)
+        {
+            p2CurrentTime = 0;
+            gameOver = true;
+            GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+            GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = "white is the winner";        
+        }
+        p1Time.text = p1CurrentTime.ToString("0");
+        p2Time.text = p2CurrentTime.ToString("0");
+    }
+
+    public void Winner (string playerWinner)
+    {   
+        gameOver = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = playerWinner + " is the winner";
+    }
+
+    IEnumerator LoadMainMenu()
+    {
+        //GM.nickname.text = "";
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("GameMenu");
+    }
+
+    public void Surrender1()
+    {
+        gameOver = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text ="white is the winner";
+    }
+
+    public void Surrender2()
+    {
+        gameOver = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().enabled = true;
+        GameObject.FindGameObjectWithTag("WinnerText").GetComponent<Text>().text = "black is the winner";
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public firebaseScript db;
+    public InputField nickname;
+    public Button robot, traditional;
+    public Slider progress;
+    public static float counter = 0f;
+
+
+    string path = "https://aidanharringtonunity.firebaseio.com/";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+       
+        Camera.main.gameObject.AddComponent<firebaseScript>();
+        db = Camera.main.GetComponent<firebaseScript>();
+    }
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MainCamera");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "GameMenu")
+        {
+            try
+            {
+                if (nickname.text == "")
+                {
+                    robot.GetComponent<Button>().interactable = false;
+                    traditional.GetComponent<Button>().interactable = false;
+                }
+
+                else
+                {
+                    robot.GetComponent<Button>().interactable = true;
+                    traditional.GetComponent<Button>().interactable = true;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+        }
+        if (scene.name == "Game")
+        {
+            progress = FindObjectOfType<Slider>();
+
+            counter -= Time.deltaTime;
+
+            if(counter < 5f)
+            {
+                try
+                {
+                    if (progress.value < 5)
+                    {
+                        progress.value += 0.2f * Time.deltaTime;
+                    }
+                    if (progress.value == progress.maxValue)
+                    {
+                        progress.gameObject.SetActive(false);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                
+            }
+        }
+    }
+
+    public void LoadBlackScene()
+    {
+        StartCoroutine(initDB(path));
+        SceneManager.LoadScene("Game");
+        StartCoroutine(db.downloadAndSaveImage());
+        StartCoroutine(getAllBlackPieces());
+    }
+
+    public void LoadGreenScene()
+    {
+        StartCoroutine(initDB(path));
+        SceneManager.LoadScene("Game");
+        StartCoroutine(db.downloadAndSaveImage());
+        StartCoroutine(getAllGreenPieces());
+    }
+
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator clearDB()
+    {
+        yield return db.clearFirebase();
+        Application.Quit();
+    }
+
+    IEnumerator initDB(string path)
+    {
+        yield return db.initFirebase(path);
+    }
+
+    IEnumerator getAllBlackPieces()
+    {
+        yield return db.downloadAndSaveBlackPieces("blackQueen", this);
+        yield return db.downloadAndSaveBlackPieces("blackKnight", this);
+        yield return db.downloadAndSaveBlackPieces("blackBishop", this);
+        yield return db.downloadAndSaveBlackPieces("blackKing", this);
+        yield return db.downloadAndSaveBlackPieces("blackRook", this);
+        yield return db.downloadAndSaveBlackPieces("blackPawn", this);
+        yield return db.downloadAndSaveBlackPieces("whiteQueen", this);
+        yield return db.downloadAndSaveBlackPieces("whiteKnight", this);
+        yield return db.downloadAndSaveBlackPieces("whiteBishop", this);
+        yield return db.downloadAndSaveBlackPieces("whiteKing", this);
+        yield return db.downloadAndSaveBlackPieces("whiteRook", this);
+        yield return db.downloadAndSaveBlackPieces("whitePawn", this);
+    }
+
+    IEnumerator getAllGreenPieces()
+    {
+        yield return db.downloadAndSaveGreenPieces("blackQueen", this);
+        yield return db.downloadAndSaveGreenPieces("blackKnight", this);
+        yield return db.downloadAndSaveGreenPieces("blackBishop", this);
+        yield return db.downloadAndSaveGreenPieces("blackKing", this);
+        yield return db.downloadAndSaveGreenPieces("blackRook", this);
+        yield return db.downloadAndSaveGreenPieces("blackPawn", this);
+        yield return db.downloadAndSaveGreenPieces("whiteQueen", this);
+        yield return db.downloadAndSaveGreenPieces("whiteKnight", this);
+        yield return db.downloadAndSaveGreenPieces("whiteBishop", this);
+        yield return db.downloadAndSaveGreenPieces("whiteKing", this);
+        yield return db.downloadAndSaveGreenPieces("whiteRook", this);
+        yield return db.downloadAndSaveGreenPieces("whitePawn", this);
+    }
+}
+
+
+
+
+
+
